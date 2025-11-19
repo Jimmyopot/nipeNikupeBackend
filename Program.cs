fe08411abc24173eaaf68c7abfcaca04d1807ce8
Jimmy Opot <jimmyopot@gmail.com>
2025-11-19 22:49:04 +0300

    Making adjustments in signalR code for it to be instantaneous

--- Program.cs
+++ Program.cs
@@ -23,7 +23,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
-              .AllowAnyMethod();
+              .AllowAnyMethod()
+              .AllowCredentials();
     });
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Services/NameUserIdProvider.cs
+++ Services/NameUserIdProvider.cs
@@ -3,14 +3,26 @@ using System.Security.Claims;
 
 namespace NipeNikupe.Services
 {
-    // Tell SignalR which claim to use as the user identifier (your token sets "userId")
     public class NameUserIdProvider : IUserIdProvider
     {
         public string? GetUserId(HubConnectionContext connection)
         {
-            // the JwtTokenService adds a "userId" claim; fall back to NameIdentifier
-            return connection.User?.FindFirst("userId")?.Value
-                ?? connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            // This should match the claim name you use in JWT
+            return connection.User?.FindFirst("userId")?.Value;
         }
     }
 }
+
+//namespace NipeNikupe.Services
+//{
+//    // Tell SignalR which claim to use as the user identifier (your token sets "userId")
+//    public class NameUserIdProvider : IUserIdProvider
+//    {
+//        public string? GetUserId(HubConnectionContext connection)
+//        {
+//            // the JwtTokenService adds a "userId" claim; fall back to NameIdentifier
+//            return connection.User?.FindFirst("userId")?.Value
+//                ?? connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//        }
+//    }
+//}
